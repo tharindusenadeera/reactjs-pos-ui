243079c2c0070f2b7a2dcddf55aae663dbbd330f
tharindusenadeera <s.tharindu@directfn.com>
2021-07-01 11:54:57 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Dashboard } from './pages/Dashboard';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App>
+      <Router>
+        <Switch>
+          <React.Fragment>
+            <Route exact path="/" component={Dashboard} />
+          </React.Fragment>
+        </Switch>
+      </Router>
+    </App>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export const Dashboard = props => {
+    return (
+        <Fragment>
+            <h2>Develop here ...</h2>
+        </Fragment>
+    )
+}
