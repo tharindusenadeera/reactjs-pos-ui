935b54d864bd0e8097bb4ac64976f8d0f71d67e8
janithakuruppu <janithakuruppu@gmail.com>
2021-07-04 20:33:52 +0530

    Layout start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Dashboard } from './pages/Dashboard';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Dashboard } from "./pages/Dashboard";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,7 +19,7 @@ ReactDOM.render(
       </Router>
     </App>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,50 @@
-import React, { Fragment } from 'react';
+import React from "react";
+import styled from "styled-components";
 
-export const Dashboard = props => {
-    return (
-        <Fragment>
-            <h2>Develop here ...</h2>
-        </Fragment>
-    )
-}
+const Wrapper = styled.div`
+  .container,
+  .container-fluid,
+  .container-lg,
+  .container-md,
+  .container-sm,
+  .container-xl {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+`;
+
+const Header = styled.header``;
+
+const Footer = styled.footer``;
+
+const Section = styled.section``;
+
+export const Dashboard = (props) => {
+  return (
+    <Wrapper>
+      <Header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">Header</div>
+          </div>
+        </div>
+      </Header>
+      <Section>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-xl-4 order-3 order-xl-1">Left</div>
+            <div className="col-5 order-1 order-xl-2">Middle</div>
+            <div className="col-3 order-2 order-xl-3">Right</div>
+          </div>
+        </div>
+      </Section>
+      <Footer>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col text-center">© All rights reserved 2021</div>
+          </div>
+        </div>
+      </Footer>
+    </Wrapper>
+  );
+};
