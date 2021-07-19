3fc968423beb29ea48252f37de72e7c63c50deb8
tharindusenadeera <s.tharindu@directfn.com>
2021-07-19 13:06:14 +0530

    Added common reducer

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,6 +1,11 @@
+//--------- Items -----------------------//
 export const ADD_ITEM = 'ADD_ITEM';
 export const UPDATE_ITEM = 'UPDATE_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
 
+//---------- Customer ------------------//
 export const ADD_CUSTOMER_TRIGGERED = 'ADD_CUSTOMER_TRIGGERED';
 export const ADD_CUSTOMER = 'ADD_CUSTOMER';
+
+//----------- Common -------------------//
+export const ERROR = 'ERROR';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import selectedItems from './selectedItems';
 import customer from './customer';
+import common from "./common";
 
 export default combineReducers({
     selectedItems,
-    customer
+    customer,
+    common
 });
