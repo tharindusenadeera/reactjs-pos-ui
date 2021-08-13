64008e91355d2386474303408af3e3b00165563a
kasun_vimu <kasunvimu8@gmail.com>
2021-08-13 21:44:29 +0530

    Meal type changes with selected order
    Meal type changes with selected order

--- src/sections/pos/CatagorySection.js
+++ src/sections/pos/CatagorySection.js
@@ -8,15 +8,15 @@ import * as Constants from "../../constants/Constants";
 export const CatagorySection = (props) => {
   const dispatch = useDispatch();
   const mealType = useSelector((state) => state.common.mealType);
-  const [value, setValue] = React.useState(mealType);
+  // const [value, setValue] = React.useState(mealType);
 
   const handleMealType = (e) => {
-    setValue(e.target.value);
+    // setValue(e.target.value);
     dispatch(addMealType(e.target.value));
   };
   return (
     <Fragment>
-      <RadioCustom onChange={handleMealType} value={value}>
+      <RadioCustom onChange={handleMealType} value={mealType}>
         {Constants.mealTypes.map((item, key) => {
           return <Radio key={key} value={item.key}>{item.value}</Radio>;
         })}
