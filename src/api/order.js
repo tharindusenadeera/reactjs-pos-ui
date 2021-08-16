7a7d4243218235f549283993c17cc66a5319f867
tharindusenadeera <s.tharindu@directfn.com>
2021-08-16 18:54:31 +0530

    Login implemented

--- src/api/order.js
+++ src/api/order.js
@@ -1,23 +1,53 @@
 import axios from "axios";
 
 export const getAllOrders = (params = {}) => {
-  return axios.get(`${process.env.REACT_APP_API_URL}/v1/orders`, {
-    params,
-  });
+  return axios.get(
+    `${process.env.REACT_APP_API_URL}/v1/orders`,
+    {
+      headers: {
+        Authorization: localStorage.access_token,
+      },
+    },
+    params
+  );
 };
 
 export const addOrder = (newOrder) => {
-  return axios.post(`${process.env.REACT_APP_API_URL}/v1/new-order`, newOrder);
+  return axios.post(`${process.env.REACT_APP_API_URL}/v1/new-order`, newOrder, {
+    headers: {
+      Authorization: localStorage.access_token,
+    },
+  });
 };
 
 export const getOrder = (id) => {
-  return axios.get(`${process.env.REACT_APP_API_URL}/v1/get-order/${id}`);
-}
+  return axios.get(`${process.env.REACT_APP_API_URL}/v1/get-order/${id}`, {
+    headers: {
+      Authorization: localStorage.access_token,
+    },
+  });
+};
 
 export const updateOrder = (id, updatedOrder) => {
-  return axios.post(`${process.env.REACT_APP_API_URL}/v1/edit-order/${id}`, updatedOrder);
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/v1/edit-order/${id}`,
+    updatedOrder,
+    {
+      headers: {
+        Authorization: localStorage.access_token,
+      },
+    }
+  );
 };
 
 export const placePayment = (id, data) => {
-  return axios.post(`${process.env.REACT_APP_API_URL}/v1/settled_payment/${id}`, data);
-}
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/v1/settled_payment/${id}`,
+    data,
+    {
+      headers: {
+        Authorization: localStorage.access_token,
+      },
+    }
+  );
+};
