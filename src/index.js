f07a81e6320a00e51c0d6a991860270a4da45acb
tharindusenadeera <s.tharindu@directfn.com>
2021-08-26 09:54:49 +0530

    Hash route added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
